3fe25e6f8d85a052b3350c563edd1db9ae02937d
Nadia Ligia <nlnadialigia@gmail.com>
2022-02-12 20:46:58 -0300

    feat($semana01): iniciar api com express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Start');
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const port = 5000;
+
+app.listen(process.env.port || port, () => {
+  console.log(`Servidor em execução na port: ${port}`);
+});
