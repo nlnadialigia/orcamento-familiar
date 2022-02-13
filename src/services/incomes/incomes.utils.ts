605792a1830753b05496000b90012db6e4abe60e
Nadia Ligia <nlnadialigia@gmail.com>
2022-02-13 18:42:20 -0300

    feat($semana01): criaçao da funcionalidade de deletar receita

--- src/services/incomes/incomes.utils.ts
+++ src/services/incomes/incomes.utils.ts
@@ -3,7 +3,7 @@ import Income from '../../models/incomes.model';
 
 const getIncomesList = async (req: Request, res: Response) => {
   try {
-    const incomes = await Income.find({}, '-_id -__v');
+    const incomes = await Income.find({}, '-__v');
 
     if (incomes.length === 0) {
       res.json({ MESSAGE: 'Nenhum registro encontradao' });
@@ -49,23 +49,63 @@ const findIncomeById = async (req: Request, res: Response) => {
   try {
     const income = await Income.findOne({ _id: id });
     res.status(200).json(income);
-  } catch (err) {
-    if (err instanceof Error) {
-      if (err.name === 'CastError') {
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === 'CastError') {
         res.status(404).json({ ERRO: 'Receita não encontrada' });
         return;
       }
-      res.status(500).json({ ERRO: err });
+      res.status(500).json({ ERRO: error });
     }
   }
 };
 
 const updateIncomeById = async (req: Request, res: Response) => {
-  res.send('Rota PUT => Alterar receita por ID');
+  const { id } = req.params;
+
+  const { title, value, date } = req.body;
+
+  const income = {
+    title,
+    value,
+    date,
+  };
+
+  try {
+    await Income.updateOne({ _id: id }, income);
+
+    res.status(200).json({
+      message: 'Receita atualizada com sucesso',
+      income
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === 'CastError') {
+        res.status(404).json({ ERRO: 'Receita não encontrada' });
+        return;
+      }
+      res.status(500).json({ ERRO: error });
+    }
+  }
 };
 
 const deleteIncomeById = async (req: Request, res: Response) => {
-  res.send('Rota DELETE => Deletar receita por ID');
+  const { id } = req.params;
+
+  try {
+    await Income.findByIdAndDelete({ _id: id });
+    res.status(200).json({
+      message: 'Receita removida com sucesso!'
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === 'CastError') {
+        res.status(404).json({ ERRO: 'Receita não encontrada' });
+        return;
+      }
+      res.status(500).json({ ERRO: error });
+    }
+  }
 };
 
 export {
