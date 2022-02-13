6760266f5a7eb728b98a797557184167afa93f51
Nadia Ligia <nlnadialigia@gmail.com>
2022-02-13 19:13:26 -0300

    refactor($semana01): tratativa de erro ao criar despesa

--- src/services/expenses/expenses.utils.ts
+++ src/services/expenses/expenses.utils.ts
@@ -1,11 +1,37 @@
 import { Request, Response } from 'express';
+import Expense from '../../models/expenses.model';
 
 const getExpensesList = async (req: Request, res: Response) => {
   res.send('ok');
 };
 
 const createExpense = async (req: Request, res: Response) => {
-  res.send('ok');
+  const { title, value, date } = req.body;
+
+  const expense = {
+    title,
+    value,
+    date,
+  };
+
+  try {
+    await Expense.create(expense);
+
+    res.status(201).json({
+      message: 'Despesa criada com sucesso!',
+      expense
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === 'ValidationError') {
+        res.status(400).json({
+          message: 'Todos os campos são obrigatórios'
+        });
+        return;
+      }
+    }
+    res.status(500).json({ ERRO: error });
+  }
 };
 
 const findExpenseById = async (req: Request, res: Response) => {
