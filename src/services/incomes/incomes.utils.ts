5e79e2696f074903a0c1b3069f64e4a884863a9a
Nadia Ligia <nlnadialigia@gmail.com>
2022-02-13 16:03:36 -0300

    refactor($semana01): separacao do erro em nao encontrado e erro de servidor

--- src/services/incomes/incomes.utils.ts
+++ src/services/incomes/incomes.utils.ts
@@ -3,9 +3,9 @@ import Income from '../../models/incomes.model';
 
 const getIncomesList = async (req: Request, res: Response) => {
   try {
-    const incomes = await Income.find();
+    const incomes = await Income.find({}, '-_id -__v');
 
-    res.json(incomes);
+    res.status(200).json(incomes);
   } catch (error) {
     res.status(500).json({ erro: error });
   }
@@ -33,15 +33,19 @@ const createIncome = async (req: Request, res: Response) => {
 };
 
 const findIncomeById = async (req: Request, res: Response) => {
-  const { id } = req.params;
+  const id = req.params.id;
 
   try {
-    const income = await Income.findById({ _id: id });
-    res.json(income);
-  } catch (error) {
-    res.status(404).json({
-      message: 'Receita não encontrada!',
-    });
+    const income = await Income.findOne({ _id: id });
+    res.status(200).json(income);
+  } catch (err) {
+    if (err instanceof Error) {
+      if (err.name === 'CastError') {
+        res.status(404).json({ erro: 'Receita não encontrada' });
+        return;
+      }
+      res.status(500).json({ erro: err });
+    }
   }
 };
 
