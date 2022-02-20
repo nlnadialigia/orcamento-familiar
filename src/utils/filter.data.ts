60570e82a88758134556bde7a783c3d3fd5f5e69
Nadia Ligia <nlnadialigia@gmail.com>
2022-02-20 19:32:47 -0300

    refactor($semana02): alteracao da variavel de data

--- src/utils/filter.data.ts
+++ src/utils/filter.data.ts
@@ -1,11 +1,11 @@
 import { Month, Year } from '.';
 
 async function FilterDate(model: any, year: number, month: number) {
-  const incomes = await model.find({});
+  const data = await model.find({});
   const newData = [];
 
-  for (let i = 0; i < incomes.length; i++) {
-    const element = incomes[i];
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
     const elementMonth = Month(element.date);
     const elementYear = Year(element.date);
     if (elementMonth === month && elementYear === year) {
