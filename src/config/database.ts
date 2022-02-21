e5e17d223a64efead8c617010c9d61de5c6df992
Nadia Ligia <nlnadialigia@gmail.com>
2022-02-20 21:57:32 -0300

    refactor($codacy): fix codacy issues

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import 'dotenv/config';
 
 const env = process.env;
--- src/utils/custom.date.ts
+++ src/utils/custom.date.ts
@@ -1,11 +1,11 @@
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
-function Month(date: moment.MomentInput) {
+function Month(date: moment.MomentInput): number {
   return moment.utc(date).get('month') + 1;
 }
 
-function Year(date: moment.MomentInput) {
+function Year(date: moment.MomentInput): number {
   return moment.utc(date).get('year') + 1;
 }
 
